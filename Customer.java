b34e5e08ef52755a1a2c2f6d6ecb16cefd1c59af
mk1035953 <mk1035953@wcupa.edu>
2025-11-30 21:34:35 -0500

    Delete Customer.java

--- Customer.java
+++
@@ -1,8 +0,0 @@
-public class Customer extends User{
-    public Customer(String user, String pass){
-        super(user, pass);
-    }
-
-    //Accessors
-    public String getUserType(){return "Customer";}
-}
