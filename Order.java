bd98fb4b01fefcf355c278ecfbe5006418fbf463
TylerBagent <tb1001799@wcupa.edu>
2025-11-30 22:13:57 -0500

    Implemented Order class     writeToFile method     Cart ArrayList

--- Order.java
+++ Order.java
@@ -1,13 +1,25 @@
+import java.io.FileWriter;
+import java.util.ArrayList;
+
 public class Order {
     private String user;
     private ArrayList<Item> Cart;
 
-    //Order Class
-    public Order(){
+        //Order Class
+        public Order(){
+            Cart = new ArrayList<>();
+        }
+        //Should write order in the form: Username,Product1Id,numBought,Product2Id,numBought,...
+        public void writeToFile(){
+            String filename = user + "_order.csv";
+            try(FileWriter writer = new FileWriter(filename, true)){
+                writer.write(user);
 
-    }
-    //Should write order in the form: Username,Product1Id,numBought,Product2Id,numBought,...
-    public void writeToFile(){
-
+                for (Item item : Cart){
+                    writer.write("," + item.getID() + "," + item.getItemsLeft());
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+        }
     }
 }
