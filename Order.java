5910d469432cbc304558a7226b568f50e56ba57a
mk1035953 <mk1035953@wcupa.edu>
2025-11-30 21:40:11 -0500

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,13 @@
+public class Order {
+    private String user;
+    private ArrayList<Item> Cart;
+
+    //Order Class
+    public Order(){
+
+    }
+    //Should write order in the form: Username,Product1Id,numBought,Product2Id,numBought,...
+    public void writeToFile(){
+
+    }
+}
